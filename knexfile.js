fa4c5726b49a9cbc99a2db01d43a7dac94239520
Brady Bridges <bradyjbridges@gmail.com>
2020-04-02 15:30:15 -0600

    Formatting

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,4 @@
 module.exports = {
-
   development: {
     client: 'pg',
     connection: 'postgres://localhost/pi_proximity_api',
@@ -10,5 +9,16 @@ module.exports = {
       directory: './db/seeds/dev'
     },
     useNullAsDefault: true
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    useNullAsDefault: true,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    }
   }
 };
